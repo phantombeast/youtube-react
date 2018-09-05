e36a74033b148fdda799272e0f432c2d41360513
phantombeast <rajpalpulkit12@gmail.com>
2018-09-05 23:42:45 +0530

    video list added

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,29 @@
-import React from 'react';
+import React,{Component} from 'react';
+//const COmpoenet=React.Componenet;
+class SearchBar extends React.Component{
+    constructor(props) {
+        super(props);
 
-const SearchBar=()=>{
-    return <input/>;
+        this.state = {term: ''};
+    }
+        render()
+        {
+            return(
+            <div>
+             <input value={this.state.term}
+                 onChange={event => this.setState({term:event.target.value})}/>;
+
+            </div>
+            );
+            // value : input becomes controlled compo,,
+        }
+
+        // onInputChange(ev)
+        // {
+        //
+        //     console.log(ev.target.value);
+        // }
+
 }
 
 export default SearchBar;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, {Component} from 'react'
 import ReactDOM from 'react-dom';
 import SearchBar from './components/searchbar'
-const api_key=AIzaSyBQizGVtr1bcDiYAb4kgTglG-BfYkHSrtk;
+import ytSearch from 'youtube-api-search'
+import VideoList from './components/video_list'
+
+const api_key='AIzaSyBQizGVtr1bcDiYAb4kgTglG-BfYkHSrtk';
+
+ytSearch({key:api_key, term:'surfboard'}, (data)=>{
+    console.log(data);
+} )
 
 // create component
 // it should produce some html
 
 
 
-const App=function () {
-    return <div>
-        <SearchBar/>
-    </div>
+class App extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {videos: []};
+
+        ytSearch({key:api_key, term:'surfboard'}, (videos)=>{
+            this.setState({videos})
+            // this.setState({videos:data})
+        } )
+    }
+
+    render()
+    {
+        return (<div>
+                <SearchBar/>
+                <VideoList videos={this.state.videos}/>
+            </div>
+        )
+    }
 }
 
 // take this code output i.e  html and put it on dom html
